0d05ef5ef5e9cba2321cc3f2c4977ca17743f3e2
renatosilva2022 <107969416+renatosilva2022@users.noreply.github.com>
2024-06-05 09:20:58 -0300

    desafio.py

--- desafio.py
+++ desafio.py
@@ -1,12 +1,9 @@
 #######################
 # Import libraries
-
 import streamlit as st
 import pandas as pd
 import altair as alt
 import plotly.express as px
-
-
 
 #######################
 # Page configuration
