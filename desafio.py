1c3a2632708d7b1acf01bcb439f9c2bee0e8d3c6
renatosilva2022 <107969416+renatosilva2022@users.noreply.github.com>
2024-06-05 09:18:52 -0300

    Add files via upload

---
+++ desafio.py
@@ -0,0 +1,119 @@
+#######################
+# Import libraries
+
+import streamlit as st
+import pandas as pd
+import altair as alt
+import plotly.express as px
+
+
+
+#######################
+# Page configuration
+st.set_page_config(
+    page_title="US Population Dashboard",
+    page_icon="🏂",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+alt.themes.enable("dark")
+
+#######################
+# CSS styling
+st.markdown("""
+<style>
+
+[data-testid="block-container"] {
+    padding-left: 2rem;
+    padding-right: 2rem;
+    padding-top: 1rem;
+    padding-bottom: 0rem;
+    margin-bottom: -7rem;
+}
+
+[data-testid="stVerticalBlock"] {
+    padding-left: 0rem;
+    padding-right: 0rem;
+}
+
+[data-testid="stMetric"] {
+    background-color: #393939;
+    text-align: center;
+    padding: 15px 0;
+}
+
+[data-testid="stMetricLabel"] {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+[data-testid="stMetricDeltaIcon-Up"] {
+    position: relative;
+    left: 38%;
+    -webkit-transform: translateX(-50%);
+    -ms-transform: translateX(-50%);
+    transform: translateX(-50%);
+}
+
+[data-testid="stMetricDeltaIcon-Down"] {
+    position: relative;
+    left: 38%;
+    -webkit-transform: translateX(-50%);
+    -ms-transform: translateX(-50%);
+    transform: translateX(-50%);
+}
+
+</style>
+""", unsafe_allow_html=True)
+
+
+# st.set_page_config(layout="wide")
+
+# Dashboard Financeiro 
+# Com uma visão mensal
+# Faturamento por unidade… 
+# Tipo de produto mais vendido, contribuição por filial,
+# Desempenho das forma de pagamento,
+# Avaliação das filiais
+
+st.title('Projeto Desenvolve - Renato de Almeida Silva - 462\n')
+st.title("Dashboard Financeiro")
+
+
+df = pd.read_csv("supermercado.csv", sep=";", decimal=",")
+df["Date"] = pd.to_datetime(df["Date"])
+df=df.sort_values("Date")
+
+df["Month"] = df["Date"].apply(lambda x: str(x.year) + "-" + str(x.month))
+month = st.sidebar.selectbox("Mês", df["Month"].unique())
+
+df_filtered = df[df["Month"] == month]
+
+col1, col2 = st.columns(2)
+col3, col4, col5 = st.columns(3)
+
+fig_date = px.bar(df_filtered, x="Date", y="Total", color="City", title="Faturamento por dia")
+col1.plotly_chart(fig_date, use_container_width=True)
+
+fig_prod = px.bar(df_filtered, x="Date", y="Product line", 
+                  color="City", title="Faturamento por tipo de produto",
+                  orientation="h")
+col2.plotly_chart(fig_prod, use_container_width=True)
+
+
+city_total = df_filtered.groupby("City")[["Total"]].sum().reset_index()
+fig_city = px.bar(city_total, x="City", y="Total",
+                   title="Faturamento por filial")
+col3.plotly_chart(fig_city, use_container_width=True)
+
+
+fig_kind = px.pie(df_filtered, values="Total", names="Payment",
+                   title="Faturamento por tipo de pagamento")
+col4.plotly_chart(fig_kind, use_container_width=True)
+
+
+city_total = df_filtered.groupby("City")[["Rating"]].mean().reset_index()
+fig_rating = px.bar(df_filtered, y="Rating", x="City",
+                   title="Avaliação")
+col5.plotly_chart(fig_rating, use_container_width=True)
